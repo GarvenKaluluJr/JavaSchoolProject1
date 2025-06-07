ebb64e28e3fe78a64189882f090610d582eb8340
GarvenKalulu <garvenkalulu2000@gmail.com>
2025-06-07 04:14:43 +0300

    Step 6: Implement caching(Redis)

--- taskapi/src/main/java/com/java/taskapi/TaskapiApplication.java
+++ taskapi/src/main/java/com/java/taskapi/TaskapiApplication.java
@@ -2,13 +2,12 @@ package com.java.taskapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TaskapiApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(TaskapiApplication.class, args);
 	}
-
 }
